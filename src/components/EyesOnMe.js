3341735b774e6d6e3aec8fda809ec2c7a1281229
misskip <anita.desanmiguel@gmail.com>
2021-12-31 16:24:49 +1100

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+    return (
+    <input
+        onFocus={(e) => {
+          console.log('Good!');
+        }}
+        placeholder="text"
+    />
+    )
+  }
+ /* 
+  function Example() {
+    return (
+    <input
+        onBlur={(e) => {
+          console.log('Hey! Eyes on me!');
+        }}
+        placeholder="text"
+    />
+    )
+  }
+ */ 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+    function handleChange(e) {
+        console.log(`${e.target.value}`);
+    }
+    
+    return (
+        <div>
+        <form heading="Keypad" >
+            <input
+            type= "thepasswordis"
+            onChange={handleChange}/>
+        </form>
+          
+        </div>
+    )
+}
+export default Keypad;
